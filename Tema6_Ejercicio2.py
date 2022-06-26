d1df06a44810a4a3c7f1d76e4c87507849495d4a
Alejandro <alejandrosaezc@gmail.com>
2022-06-26 16:01:44 +0200

    Tema6_Ejercicio2

--- Tema6_Ejercicio2.py
+++ Tema6_Ejercicio2.py
@@ -1,6 +1,30 @@
 class Alumno:
     def __init__(self):
-        print("constructor")
+        self._nombre = None
+        self._nota = None
+
+    def inicializar(self, nombre, nota):
+        self._nombre = nombre
+        self._nota = nota
+
+    def imprimir(self):
+        print("El alumno " + self._nombre + " tiene una nota de " + str(self._nota))
+
+    def aprobado(self):
+        if self._nota>=5:
+            print("El alumno ha aprobado")
+        else:
+            print("El alumno ha suspendido")
 
 
-p = Alumno()
+alumno1 = Alumno()
+alumno2 = Alumno()
+
+alumno1.inicializar("Alejandro", 8)
+alumno2.inicializar("Paco", 2)
+
+alumno1.imprimir()
+alumno2.imprimir()
+
+alumno1.aprobado()
+alumno2.aprobado()
