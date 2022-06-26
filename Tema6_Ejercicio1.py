5b1a5c05d8f4d8842ba5339110b01ddcf50fb15b
Alejandro <alejandrosaezc@gmail.com>
2022-06-26 15:54:12 +0200

    Tema6_Ejercicio1

--- Tema6_Ejercicio1.py
+++ Tema6_Ejercicio1.py
@@ -1,15 +1,20 @@
 class Vehiculo:
-    _color = 'Blanco'
-    _ruedas = 4
-    _puertas = 4
+    def __init__(self, color, ruedas, puertas):
+        self._color = color
+        self._ruedas = ruedas
+        self._puertas = puertas
 
 
 class Coche(Vehiculo):
-    _velocidad = 100
-    _cilindrada = 500
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self._velocidad = velocidad
+        self._cilindrada = cilindrada
+
+    def __str__(self):
+        return "Color: " + self._color + ", ruedas: " + str(self._ruedas) + ", puertas: " + str(
+            self._puertas) + ", velocidad: " + str(self._velocidad) + ", cilindrada: " + str(self._cilindrada)
 
 
-c = Coche
-print(c._velocidad)
-print(c._color)
-print(c._puertas)
+c = Coche("Blanco", 4, 4, 100, 500)
+print(c)
