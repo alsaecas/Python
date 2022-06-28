d1987c80fcfcd60cc6b6f4ab81b78f4d834bc3d7
Alejandro <alejandrosaezc@gmail.com>
2022-06-28 16:39:58 +0200

    Tema10_Ejercicio1

--- Tema10_Ejercicio1.py
+++ Tema10_Ejercicio1.py
@@ -4,20 +4,27 @@ from tkinter import ttk
 if __name__ == '__main__':
     def reset():
         seleccionado.set(None)
+        texto.config(text='')
+
+    def seleccionar():
+        texto.config(text=seleccionado.get())
 
 
     window = tkinter.Tk()
 
     seleccionado = tkinter.StringVar()
-    r1 = ttk.Radiobutton(window, text='Option 1', value='1', variable=seleccionado)
-    r2 = ttk.Radiobutton(window, text='Option 2', value='2', variable=seleccionado)
-    r3 = ttk.Radiobutton(window, text='Option 3', value='3', variable=seleccionado)
+    r1 = ttk.Radiobutton(window, text='Option 1', value='Option 1', variable=seleccionado, command=seleccionar)
+    r2 = ttk.Radiobutton(window, text='Option 2', value='Option 2', variable=seleccionado, command=seleccionar)
+    r3 = ttk.Radiobutton(window, text='Option 3', value='Option 3', variable=seleccionado, command=seleccionar)
 
     r1.grid(column=0, row=1, pady=5, padx=5)
     r2.grid(column=0, row=2, pady=5, padx=5)
     r3.grid(column=0, row=3, pady=5, padx=5)
 
+    texto = ttk.Label(window, text=seleccionado.get())
+    texto.grid(column=3, row=1, pady=5, padx=5)
+
     boton = ttk.Button(window, text='Reset', command=reset)
-    boton.grid(column=3, row=1, pady=5, padx=5)
+    boton.grid(column=3, row=3, pady=5, padx=5)
 
     window.mainloop()
